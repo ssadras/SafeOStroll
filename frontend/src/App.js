cc87ef9525ba8aea375db1915e1fdae29c796d00
arinaazmi <arina.cat2003@gmail.com>
2024-10-05 00:16:59 -0400

    login signup ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Login from './components/login';
+import Signup from './components/signup';
 import './App.css';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleAuthMode = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {isLogin ? <Login /> : <Signup />}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {isLogin ? "Don't have an account?" : 'Already have an account?'}
+          <button onClick={toggleAuthMode}>
+            {isLogin ? 'Sign up here' : 'Log in here'}
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
       </header>
     </div>
   );
