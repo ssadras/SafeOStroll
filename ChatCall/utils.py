32df8c21a89debd0131f1d54eddc3fb5f8479368
Arshiya <arshiya.mahmoodinezhad@mail.utoronto.ca>
2024-10-05 09:59:40 -0400

    'minor change to util'

--- ChatCall/utils.py
+++ ChatCall/utils.py
@@ -6,14 +6,13 @@ def generate_response(input_text):
         openai.api_key = settings.SECRET_AI_KEY  # Set your OpenAI API key
 
         # Make a call to the OpenAI API
-        response = openai.chat.completions.create(
+        response = openai.ChatCompletion.create(
             model="gpt-4o",  # Specify the correct model (like GPT-4 or GPT-3.5)
             messages=[
                 {"role": "user", "content": input_text}
             ]
         )
         
-        # Correct way to access the message content
         bot_message = response.choices[0].message['content']
         return bot_message
     except Exception as e:
