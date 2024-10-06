ea598885d2bdcdb5b72e62dc4f04095bfc53c468
Sadra <sadra3tarehdan@gmail.com>
2024-10-06 02:26:45 -0400

    Chat History clearing system (Backend)

--- ChatCall/urls.py
+++ ChatCall/urls.py
@@ -1,6 +1,6 @@
 # myapp/urls.py
 from django.urls import path
-from .views import ai_chat_view, ai_tts, ai_stt
+from .views import ai_chat_view, ai_tts, ai_stt, clear_chat_history
 
 app_name = "ChatCall"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('ai_chat/', ai_chat_view, name='ai_chat'),
     path('ai_tts/', ai_tts, name='ai_tts'),
     path('ai_stt/', ai_stt, name='ai_stt'),
+    path('clear_history/', clear_chat_history, name='clear_history'),
 ]
