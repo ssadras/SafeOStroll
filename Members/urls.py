928826a49abedb5c0c830b0a70329a84a5c59450
Arshiya <arshiya.mahmoodinezhad@mail.utoronto.ca>
2024-10-06 01:18:26 -0400

    'Started implemenmting nearby member marker'

--- Members/urls.py
+++ Members/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', SignupView.as_view(), name='register'),
     path('set-location/', SetLocationView.as_view(), name='set_location'),
-    #path('get-members-around-user/', GetMembersAroundUser.as_view(), name='get_members_around_user'),
+    path('get-members-around-user/', GetMembersAroundUser.as_view(), name='get_members_around_user'),
     #path('ask-help-nearby-members/', AskHelpNearbyMembers.as_view(), name='ask_help_nearby_members'),
 ]
